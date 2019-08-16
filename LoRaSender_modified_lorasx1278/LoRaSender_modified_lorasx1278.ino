e55f2801073ea9253186d8907d67305d592e732d
Nelson Javier Castillo Ordenes <33573110+Labmec2s2017@users.noreply.github.com>
2019-08-16 13:23:11 -0400

    Update LoRaSender_modified_lorasx1278.ino

--- LoRaSender_modified_lorasx1278/LoRaSender_modified_lorasx1278.ino
+++ LoRaSender_modified_lorasx1278/LoRaSender_modified_lorasx1278.ino
@@ -1,7 +1,28 @@
+//programacion autor : Nelson Castillo
+// ------------------EMISOR-------------------------------------------------------
+//descripcion codigo : codigo que envia un hello y un contador a un micro receptor,se implementa en STM32f103 CON LORA RA-02
+//la antena utilizada es un resorte que puse(mejoro la señal).
+//pinout de conexion :   STM32   ------------- RA-02              
+//                       GND --------------->  GND
+//                       3,3V--------------->  3,3V
+//                       PA1 --------------->  DIO0
+//                       PA4---------------->  NSS
+//                       PA5 --------------->  SCK
+//                       PA6 --------------->  MISO
+//                       PA7 --------------->  MOSI
+//                       PC14 ---------------> RST
+//                       PB13 ---------------> DIO1
+//                       PB12 ---------------> DIO3
+//---------------------------------------------------------------------------------
+//PINOUT DE CONEXION :    STM32 --------------FTDI (USB)
+//                        GND   -------------> GND
+//                        3,3V  -------------> 3,3V   PUEDE SER 5 VOLT EN AMBOS IGUAL
+//                        PA9  --------------> RX
+//                        PA10 --------------> TX
 #include <SPI.h>
-#include <LoRa_STM32.h>
+#include <LoRa_STM32.h>  //libreria necesaria para la comunicacion
 
-int counter = 0;
+int counter = 0;          // definir contador como un numero entero en valor cero inicial.
 const int csPin = PA4;//7;          // LoRa radio chip select
 const int resetPin = PC14;//6;       // LoRa radio reset
 const int irqPin = PA1;//1;         // change for your board; must be a hardware interrupt pin
@@ -11,30 +32,30 @@ void setup() {
   while (!Serial);
 
   Serial.println("LoRa Sender");
-   // override the default CS, reset, and IRQ pins (optional)
-  LoRa.setPins(csPin, resetPin, irqPin);// set CS, reset, IRQ pin
-   LoRa.setTxPower(14, PA_OUTPUT_RFO_PIN);//PA_OUTPUT_PA_BOOST_PIN);
-  LoRa.setSignalBandwidth(41.7E3); 
-  LoRa.setSpreadingFactor(12);
-  LoRa.setCodingRate4(5);
+   // definicion de pines del SPI / override the default CS, reset, and IRQ pins (optional)
+  LoRa.setPins(csPin, resetPin, irqPin);      // set CS, reset, IRQ pin
+   LoRa.setTxPower(14, PA_OUTPUT_RFO_PIN);    //PA_OUTPUT_PA_BOOST_PIN);
+  LoRa.setSignalBandwidth(41.7E3);            //DEFINIR ancho de banda
+  LoRa.setSpreadingFactor(12);                //definir spreding factor
+  LoRa.setCodingRate4(5);                     // definir coding rate
   
-  if (!LoRa.begin(433E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
+  if (!LoRa.begin(433E6)) {                    //los modulos de 433mhz depende del fabricante
+    Serial.println("Starting LoRa failed!");   //mensaje sino es exitisa la comunicacion con lora
+    while (1);                  
   }
 }
 
 void loop() {
-  Serial.print("Sending packet: ");
-  Serial.println(counter);
+  Serial.print("Sending packet: ");    //MENSAJE X SERIAL DE ENVIO DE DATOS
+  Serial.println(counter);             //MUESTRA X SERIAL EL NUMERO CONTADOR
 
-  // send packet
+  // ENVIO DE PAQUETES DE DATOS / send packet
   LoRa.beginPacket();
   LoRa.print("hello ");
   LoRa.print(counter);
   LoRa.endPacket();
 
-  counter++;
+  counter++;  //EL CONTADOR SE AUMENTA EN 1 CADA VEZ 
 
-  delay(5000);
+  delay(5000);   // RETARDO DE 5 SEGUNDOS
 }
